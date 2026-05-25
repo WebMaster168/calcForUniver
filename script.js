dad3201935c2d83b46ba37ae73904e1b8f6505e4
ilya <shcherbakov.illya@list>
2026-05-25 17:03:59 +0400

    calculator

---
+++ script.js
@@ -0,0 +1,24 @@
+const display = document.getElementById("display");
+ 
+function append(value) {
+  display.value += value;
+}
+ 
+function clearDisplay() {
+  display.value = "";
+}
+ 
+function deleteLast() {
+  display.value = display.value.slice(0, -1);
+}
+ 
+function calculate() {
+  try {
+    let expression = display.value;
+
+    expression = expression.replace(/(\d+)%/g, "($1/100)")
+    display.value = eval(expression);
+  } catch {
+    display.value = "Error";
+  }
+}
